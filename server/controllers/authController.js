9ea240c6ed0ec6b6d52e608e5335a711d1d25e14
Sidhiq Ali <sidhiqsidhiqali@gmail.com>
2023-06-06 16:39:39 +0530

    404error

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -50,7 +50,11 @@ export const register = async (req, res, next) => {
 
     const { password: _, ...info } = newUser._doc;
     res
-      .cookie('accessToken', token, { httpOnly: true })
+      .cookie('accessToken', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      })
       .status(201)
       .send({ info, token });
   } catch (error) {
@@ -76,7 +80,14 @@ export const login = async (req, res, next) => {
     );
 
     const { password, ...info } = user?._doc;
-    res.cookie('accessToken', token, { httpOnly: true }).status(200).send(info);
+    res
+      .cookie('accessToken', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      })
+      .status(200)
+      .send(info);
   } catch (error) {
     next(error);
   }
