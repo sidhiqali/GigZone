db7ac8f9126da6e0cfe6ed70bb4f1e6308d3255e
Sidhiq Ali <sidhiqsidhiqali@gmail.com>
2023-06-09 15:06:18 +0530

    stripe

--- client/src/pages/CheckoutForm.jsx
+++ client/src/pages/CheckoutForm.jsx
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: 'http://localhost:5173/success',
+        return_url: 'https://gigzone.netlify.app/success',
       },
     });
 
