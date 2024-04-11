7bc47802e2f5552f294e283148431c00514c2754
SIDHIQ ALI <sidhiqsidhiqali@gmail.com>
2024-04-11 18:36:20 +0530

    Update ProjectCard.jsx

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -12,6 +12,7 @@ const ProjectCard = ({ project }) => {
       return newRequest(`/user/${project?.userId}`).then((res) => res.data);
     },
   });
+  
   return (
     <Link to={`/gig/${project?._id}`} key={project?._id}>
       <div className='shadow-slate-300  shadow-lg w-full sm:w-3/4 md:w-10/12 lg:w-3/4 xl:w-10/12 p-2'>
