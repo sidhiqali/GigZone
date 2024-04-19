f63befc72112e3277eb8f45c8d6f09abc98e441e
SIDHIQ ALI <sidhiqsidhiqali@gmail.com>
2024-04-19 19:14:52 +0530

    Update CategorySection.jsx

--- client/src/components/CategorySection.jsx
+++ client/src/components/CategorySection.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Categories } from '../constants/data';
 import { useNavigate } from 'react-router-dom';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 const CategorySection = () => {
   const navigate = useNavigate();
   const handleNavigation = (title) => {
